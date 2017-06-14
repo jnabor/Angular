666274d6a2ee4c8d6fc08a4f8b5628e5231b1904
Jayson Nabor <sonabstudios@gmail.com>
2017-06-14 20:23:18 +0800

    2 way data binding

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
@@ -27,6 +28,7 @@ import { ImageComponent } from './image/image.component';
   imports: [
     BrowserModule,
     HttpModule,
+    FormsModule,
     BrowserAnimationsModule,
     MdButtonModule,
     MdCardModule,
